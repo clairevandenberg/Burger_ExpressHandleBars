5e5e07263c296e3c26b3f5270467258ca8ade664
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-05-22 17:02:36 +0930

    create the code that will call the ORM functions using burger specific input for the ORM

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,26 @@
 // import orm.js into burger.js
+var orm = require("../config/orm.js");
 
 // create the code that will call the ORM functions using burger specific input for the ORM.
 
+var cat = {
+  all: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burger", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burger", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
-// Export at the end of the burger.js file.
-
-var Sequelize = require("sequelize");
-
-var sequelize = require("../config/connection.js");
-
-// Creating a "Chirp" model that matches up with DB
-var NewBurger = sequelize.define("burger", {
-    burgerName: Sequelize.STRING,
-  });
-  
-  // Syncs with DB
-  NewBurger.sync();
-  
-  // Makes the Chirp Model available for other files (will also create a table)
-  module.exports = NewBurger;
+// Export the database functions for the controller (catsController.js).
+module.exports = cat;
